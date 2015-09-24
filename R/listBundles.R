e178cfcc6b1cc647f169b34a576122c2f811f204
Egon Willighagen <egon.willighagen@gmail.com>
2015-09-24 09:10:21 +0200

    Implemented listing bundles

--- R/listBundles.R
+++ R/listBundles.R
@@ -1,4 +1,14 @@
 listBundles <- function(service=NA) {
   if (missing(service)) stop("You must specify the API service to use.");
 
+  handle = new_handle()
+  handle_setheaders(handle, "User-Agent" = "r/renm")
+  handle_setheaders(handle, "Accept" = "application/json")
+
+  url = paste(service, "bundle/", sep="")
+  conn <- curl::curl(url, handle, open="r")
+  txt <- readLines(conn)
+  close(conn)
+  data = fromJSON(txt)
+
 }
