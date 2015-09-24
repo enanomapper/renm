fbf8fbadf6b0298dda49f3c543fe205e120d4dd8
Egon Willighagen <egon.willighagen@gmail.com>
2015-09-24 09:16:57 +0200

    Fixed the test description

--- tests/testthat/test-listSubstanceOwners.R
+++ tests/testthat/test-listSubstanceOwners.R
@@ -1,7 +1,7 @@
 library(citeuliker)
-context("Bundles")
+context("SubstanceOwners")
 
-test_that("bundles are found", {
+test_that("substance owners are found", {
   bundles = listSubstanceOwners("http://data.enanomapper.net/")
   expect_that(nrow(data), not(equals(0)))
 })
