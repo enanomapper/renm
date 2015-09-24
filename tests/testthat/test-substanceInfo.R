3ce4ef66f40a85af4eb273373036065f8eafb79f
Egon Willighagen <egon.willighagen@gmail.com>
2015-09-24 18:29:42 +0200

    Added substanceInfo

---
+++ tests/testthat/test-substanceInfo.R
@@ -0,0 +1,7 @@
+library(citeuliker)
+context("Substance Information")
+
+test_that("substance info is found", {
+  info = substanceInfo("https://data.enanomapper.net/substance/NWKI-71060af4-1613-35cf-95ee-2a039be0388a")
+  expect_that(nrow(info), not(equals(0)))
+})
